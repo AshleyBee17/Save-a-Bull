acb0e25982d8780bdb8d9bb20578a23d62c5f5b2
ashleybenjamin <benjamina@mail.usf.edu>
2019-02-13 12:26:04 -0500

    Beginning log in with Facebook

--- app/src/main/java/edu/usf/cse/labrador/save_a_bull/WelcomeScreen.java
+++ app/src/main/java/edu/usf/cse/labrador/save_a_bull/WelcomeScreen.java
@@ -2,6 +2,8 @@ package edu.usf.cse.labrador.save_a_bull;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeScreen extends AppCompatActivity {
 
@@ -10,10 +12,28 @@ public class WelcomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
 
+        loginClicked();
+        signUpClicked();
+        loginWithFacebookClicked();
 
 
     }
 
+    private void loginClicked() {
+    }
 
+    private void signUpClicked() {
+    }
+
+    private void loginWithFacebookClicked() {
+
+        Button facebookBtn = findViewById(R.id.facebook_btn);
+        facebookBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
 
 }
