2c061bae84589d50f79a29c1dbf8d8d7499dff90
Jahlil Milis <jahlil.milis@gmail.com>
2019-03-22 15:44:44 -0400

    new file:   app/src/main/java/edu/usf/cse/labrador/save_a_bull/AuthenticatorActivity.java 	new file:   app/src/main/java/edu/usf/cse/labrador/save_a_bull/ForgetAndChangePasswordActivity.java 	new file:   app/src/main/java/edu/usf/cse/labrador/save_a_bull/sqlite/database/model/Coupons.java 	new file:   app/src/main/java/edu/usf/cse/labrador/save_a_bull/sqlite/database/model/Deals.java 	new file:   app/src/main/res/layout/activity_forget_and_change_password.xml 	new file:   app/src/main/res/raw/awsconfiguration.json

---
+++ app/src/main/java/edu/usf/cse/labrador/save_a_bull/sqlite/database/model/Deals.java
@@ -0,0 +1,30 @@
+package edu.usf.cse.labrador.save_a_bull;
+
+import java.util.Date;
+
+public class Deals
+{
+    String Name;
+    String ImgPath;
+    String Category;
+    String Describe;
+    String Address;
+    int ZipCode;
+    String Expiration;
+
+    public Deals(String name, String category, String describe, String address, int zipCode, String expiration)
+    {
+        Name = name;
+        Category = category;
+        Describe = describe;
+        Address = address;
+        ZipCode = zipCode;
+        Expiration = expiration;
+
+    }
+
+    public Deals()
+    {
+
+    }
+}
